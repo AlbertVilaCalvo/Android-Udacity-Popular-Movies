f4589f42f6964c93f40a0f61a2d9c4bf23411fd0
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2016-05-27 19:31:50 +0200

    Add AspectRatioImageView

---
+++ app/src/main/java/eu/albertvila/popularmovies/stage2/misc/view/AspectRatioImageView.java
@@ -0,0 +1,39 @@
+package eu.albertvila.popularmovies.stage2.misc.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by albertvilacalvo on 1/11/15.
+ */
+public class AspectRatioImageView extends ImageView {
+
+    // AN ImageView THAT WILL KEEP AN ASPECT RATIO OF 1:0.675 (height:width)
+
+    // Inspired by https://gist.github.com/unosk/7b8a1c060e121978cd76
+    // and http://stackoverflow.com/a/20427215/4034572
+
+    double aspectRatioWidth = 0.657;
+    double aspectRatioHeight = 1.0;
+
+    public AspectRatioImageView(Context context) {
+        super(context);
+    }
+
+    public AspectRatioImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AspectRatioImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = getMeasuredWidth();
+        int height = (int) (width * aspectRatioHeight / aspectRatioWidth);
+        setMeasuredDimension(width, height);
+    }
+}
