e44127daf6c1c213ffc0d6112d26e63b911d9a60
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2016-05-29 22:03:21 +0200

    Add Applciation subclass

---
+++ app/src/main/java/eu/albertvila/popularmovies/stage2/misc/App.java
@@ -0,0 +1,23 @@
+package eu.albertvila.popularmovies.stage2.misc;
+
+import android.app.Application;
+
+import eu.albertvila.popularmovies.stage2.BuildConfig;
+import timber.log.Timber;
+
+/**
+ * Created by Albert Vila Calvo on 29/5/16.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+            // TODO add Stetho
+            // TODO add Leak Canary
+        }
+    }
+}
