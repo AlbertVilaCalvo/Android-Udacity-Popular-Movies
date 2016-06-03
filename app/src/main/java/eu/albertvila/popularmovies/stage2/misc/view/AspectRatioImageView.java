57df70463c3df0dd0cdf96cce0631bb4b721a2af
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2016-06-03 13:51:55 +0200

    AspectRatioImageView - add 4th constructor

--- app/src/main/java/eu/albertvila/popularmovies/stage2/misc/view/AspectRatioImageView.java
+++ app/src/main/java/eu/albertvila/popularmovies/stage2/misc/view/AspectRatioImageView.java
@@ -1,6 +1,8 @@
 package eu.albertvila.popularmovies.stage2.misc.view;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
@@ -25,8 +27,13 @@ public class AspectRatioImageView extends ImageView {
         super(context, attrs);
     }
 
-    public AspectRatioImageView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+    public AspectRatioImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public AspectRatioImageView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     @Override
