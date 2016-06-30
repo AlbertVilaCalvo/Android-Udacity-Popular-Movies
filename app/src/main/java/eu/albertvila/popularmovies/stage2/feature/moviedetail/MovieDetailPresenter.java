02229a0b090b3ec9dfdf51a197e43679cd364d07
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2016-06-30 21:02:16 +0200

    MovieDetailPresenter - implement getMovies() that subscribes it to MovieRepository observeSelectedMovie()

--- app/src/main/java/eu/albertvila/popularmovies/stage2/feature/moviedetail/MovieDetailPresenter.java
+++ app/src/main/java/eu/albertvila/popularmovies/stage2/feature/moviedetail/MovieDetailPresenter.java
@@ -1,6 +1,11 @@
 package eu.albertvila.popularmovies.stage2.feature.moviedetail;
 
+
+import eu.albertvila.popularmovies.stage2.data.model.Movie;
 import eu.albertvila.popularmovies.stage2.data.repository.MovieRepository;
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
 import timber.log.Timber;
 
 /**
@@ -10,6 +15,7 @@ public class MovieDetailPresenter implements MovieDetail.Presenter {
 
     private MovieRepository movieRepository;
     private MovieDetail.View view;
+    private Subscription subscription;
 
     public MovieDetailPresenter(MovieRepository movieRepository) {
         Timber.i("New MovieDetailPresenter created");
@@ -19,16 +25,49 @@ public class MovieDetailPresenter implements MovieDetail.Presenter {
     @Override
     public void start(MovieDetail.View view) {
         this.view = view;
+        getMovie();
     }
 
     @Override
     public void stop() {
         this.view = null;
+        unsubscribe();
     }
 
     @Override
     public void favoriteButtonClick() {
 
+    }
+
+    private void getMovie() {
+        Observable<Movie> observable = movieRepository.observeSelectedMovie();
+
+        subscription = observable.subscribe(new Subscriber<Movie>() {
+            @Override
+            public void onCompleted() {
+                Timber.i("MovieDetailPresenter getMovie() onCompleted()");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Timber.e(e, "MovieDetailPresenter getMovie() onError()");
+            }
+
+            @Override
+            public void onNext(Movie movie) {
+                Timber.i("MovieDetailPresenter getMovies() onNext() - movie: %s", movie.toString());
+                if (view != null) {
+                    view.showMovie(movie);
+                }
+            }
+        });
+    }
+
+    private void unsubscribe() {
+        if (subscription != null && !subscription.isUnsubscribed()) {
+            subscription.unsubscribe();
+            Timber.i("MovieDetailPresenter subscription.unsubscribe()");
+        }
     }
 
 }
